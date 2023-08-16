2e3db49e418efc9ab58665cd050d13f4992581b6
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-16 18:57:04 +0530

    banking html ready

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.getElementById("btn-submit").addEventListener("click", function () {
+  const emailField = document.getElementById("user-email");
+  const email = emailField.value;
+
+  const passwordField = document.getElementById("user-password");
+  const password = passwordField.value;
+
+  if (email === "mybank@bank.com" && password === "secret") {
+    window.location.href = "bank.html";
+  } else if (email === "mybank@bank.com" && password !== "secret") {
+    alert("You have entered wrong password!!😒");
+  } else if (email !== "mybank@bank.com" && password === "secret") {
+    alert("You have entered wrong email id!!😒");
+  } else if(email !== "mybank@bank.com" && password !== "secret"){
+   alert("You have entered wrong Email id & Password!! Please try again");
+  }
+});
