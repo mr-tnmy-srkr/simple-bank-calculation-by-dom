c4fda4dc79c679169a3e219f8eccedd5f796a2f8
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-08-23 22:01:46 +0530

    click on enter go to bank.html

--- js/login.js
+++ js/login.js
@@ -36,3 +36,46 @@ button2.addEventListener("click", function () {
     alert("You have entered wrong Email id & Password!! Please try again");
   }
 });
+
+// by pressing enter button
+const emailField = document.getElementById("user-email");
+const email = emailField.value;
+const passwordField = document.getElementById("user-password");
+const password = passwordField.value;
+
+passwordField.addEventListener("keyup", function (event) {
+  // console.log(event.target.value);
+  console.log(emailField.value);
+  console.log(passwordField.value);
+  console.log(event.key);
+  if (event.key === "Enter") {
+    const email = emailField.value;
+    const password = passwordField.value;
+    if (email === "mybank@bank.com" && password === "secret") {
+      window.location.href = "bank.html";
+    } else if (email === "mybank@bank.com" && password !== "secret") {
+      alert("You have entered wrong password!!😒");
+    } else if (email !== "mybank@bank.com" && password === "secret") {
+      alert("You have entered wrong email id!!😒");
+    } else if (email !== "mybank@bank.com" && password !== "secret") {
+      alert("You have entered wrong Email id & Password!! Please try again");
+    }
+  }
+});
+emailField.addEventListener("keyup", function (e) {
+  // console.log(e.target.value);
+  console.log(emailField.value);
+  console.log(passwordField.value);
+  if (e.key === "Enter") {
+    const email = emailField.value;
+    const password = passwordField.value;
+    if (email === "") {
+      alert("Please enter email");
+      return;
+    } else if (email === "mybank@bank.com" && password === "secret") {
+      window.location.href = "bank.html";
+    } else if (password === "") {
+      alert("Please enter password");
+    }
+  }
+});
